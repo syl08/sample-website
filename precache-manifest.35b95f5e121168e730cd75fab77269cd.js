173fe04c5a8ac1a9289df962fa98bdd9c63066cd
syl08 <sYinlong08@gmail.com>
2018-10-26 15:11:06 +1100

    Updates

---
+++ precache-manifest.35b95f5e121168e730cd75fab77269cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6b97bbf08fa68a92896f",
+    "url": "/sample-website/static/js/runtime~main.6b97bbf0.js"
+  },
+  {
+    "revision": "7d94e8379e07242b34bd",
+    "url": "/sample-website/static/js/main.7d94e837.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/sample-website/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "7d94e8379e07242b34bd",
+    "url": "/sample-website/static/css/main.4949d5f6.chunk.css"
+  },
+  {
+    "revision": "4a2aa2c9b19bbf36fc51b092d2eddd4d",
+    "url": "/sample-website/index.html"
+  }
+];
